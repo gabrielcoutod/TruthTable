8fe9298a10eae1d2f98e4630fb50d7d2506e626f
gabrielcoutod <50423926+gabrielcoutod@users.noreply.github.com>
2021-06-30 16:31:55 -0300

    Removing unnecessary files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="TruthTable",
+      author="Gabriel Couto Domingues",
+      license="MIT",
+      install_requires=[
+          "tabulate",
+      ],
+      zip_safe=False)
